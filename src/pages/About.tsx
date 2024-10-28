187f4816c2631f959a4792f3d114620bb4a195fa
Frank Simon <f.m.sim@t-online.de>
2024-10-28 12:48:08 +0100

    created About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,19 @@
 function About() {
-  return <h1 className="text-4xl">About Page</h1>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 justify-center items-center text-4xl sm:text-6xl font-bold leading-none tracking-wide">
+        We love{" "}
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          comfy
+        </span>
+      </h1>
+      <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 tracking-wide">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti
+        reiciendis laudantium nihil mollitia pariatur dignissimos, libero
+        blanditiis quisquam sapiente enim voluptatibus debitis minima.
+        Repudiandae necessitatibus dolore natus magnam asperiores!
+      </p>
+    </section>
+  );
 }
 export default About;

685d93093d93936f16862e3371d7377fc96c2e9b
Frank Simon <f.m.sim@t-online.de>
2024-10-29 20:06:49 +0100

    modified ProductsList.tsx

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,4 +1,36 @@
+import { formatAsDollars, type ProductsResponse } from "@/utils";
+import { Link, useLoaderData } from "react-router-dom";
+import { Card, CardContent } from "./ui/card";
+
 function ProductsList() {
-  return <div>ProductsList</div>;
+  const { data: products } = useLoaderData() as ProductsResponse;
+
+  return (
+    <div className="mt-12 grid gap-y-8">
+      {products.map((product) => {
+        const { title, price, image, company } = product.attributes;
+        const dollarsAmount = formatAsDollars(price);
+
+        return (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <Card>
+              <CardContent className="p-8 grid md:grid-cols-3 gap-y-4">
+                <img
+                  src={image}
+                  alt={title}
+                  className="w-full md:w-48 h-64 md:h-48 rounded-md object-cover"
+                />
+                <div>
+                  <h2 className="text-xl font-semibold capitalize">{title}</h2>
+                  <h4>{company}</h4>
+                </div>
+                <p className="text-primary md:ml-auto">{dollarsAmount}</p>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 export default ProductsList;
